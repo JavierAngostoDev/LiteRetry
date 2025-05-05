23f8ab03030a41ac45bc47fd941999ac626985d5
Javier Angosto <javier.angosto@gmail.com>
2025-05-05 09:36:36 +0200

    Add Retry tests

---
+++ tests/LiteRetry.Tests/RetryTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using LiteRetry.Core;
+using LiteRetry.Core.Retrying.Fluent;
+
+namespace LiteRetry.Tests;
+
+public class RetryTests
+{
+    [Fact]
+    public void Configure_ShouldReturnRetryBuilderInstance()
+    {
+        // Act
+        var builder = Retry.Configure();
+
+        // Assert
+        builder.Should().NotBeNull();
+        builder.Should().BeOfType<RetryBuilder>();
+    }
+}
